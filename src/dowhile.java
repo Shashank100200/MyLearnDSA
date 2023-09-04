3199fd805c7181e9d2f367d92c7f107e05ed3cd0
Shashank B N <shashankbn2023@gmail.com>
2023-09-04 23:56:18 +0530

    print 1-10 using do while

---
+++ src/dowhile.java
@@ -0,0 +1,9 @@
+public class dowhile {
+    public static void main(String[] args){
+        short i = 0;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i <= 10);
+    }
+}
