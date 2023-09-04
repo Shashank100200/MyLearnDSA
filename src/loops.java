953efc14e4dead057d46b7fc2398dc122f2bd2c2
Shashank B N <shashankbn2023@gmail.com>
2023-09-05 00:45:58 +0530

    Tables of numbers

--- src/loops.java
+++ src/loops.java
@@ -10,16 +10,29 @@ import java.util.*;
 
 
 //Sum of n natural numbers
+//public class loops{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the n value :");
+//        int n = sc.nextInt();
+//        int sum = 0;
+//        for(short i=0;i<=n;i++){
+//            sum = sum + i;
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+//mul tables of numbers
 public class loops{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the n value :");
-        int n = sc.nextInt();
-        int sum = 0;
-        for(short i=0;i<=n;i++){
-            sum = sum + i;
+        short n = sc.nextShort();
+        for(short i=1;i<=10;i++){
+            System.out.println(n*i);
         }
-        System.out.println(sum);
     }
 }
+
 
