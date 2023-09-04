af156cfeac026c17f232469064501ed7ccc4dc91
Shashank B N <shashankbn2023@gmail.com>
2023-09-05 00:18:52 +0530

    Sum of n natural numbers

--- src/loops.java
+++ src/loops.java
@@ -1,9 +1,23 @@
 import java.util.*;
 //for loop
+//public class loops{
+//    public static void main(String[] args){
+//        for(byte i =0;i<=10;i++){
+//            System.out.println("Reva University");
+//        }
+//    }
+//}
+
 public class loops{
     public static void main(String[] args){
-        for(byte i =0;i<=10;i++){
-            System.out.println("Reva University");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the n value :");
+        int n = sc.nextInt();
+        int sum = 0;
+        for(short i=0;i<=n;i++){
+            sum = sum + i;
         }
+        System.out.println(sum);
     }
 }
+
