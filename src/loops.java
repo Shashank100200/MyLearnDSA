497de7009d90a7dd42fca3eba6130adbb693a720
Shashank B N <shashankbn2023@gmail.com>
2023-09-04 23:48:26 +0530

    for loop

--- src/loops.java
+++ src/loops.java
@@ -1,6 +1,9 @@
 import java.util.*;
+//for loop
 public class loops{
     public static void main(String[] args){
-        System.out.println("Hey");
+        for(byte i =0;i<=10;i++){
+            System.out.println("Reva University");
+        }
     }
 }
