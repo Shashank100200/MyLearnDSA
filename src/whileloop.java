344f9033b5f85899075fa3902aed530aac85fb12
Shashank B N <shashankbn2023@gmail.com>
2023-09-04 23:51:49 +0530

    done while loop

--- src/whileloop.java
+++ src/whileloop.java
@@ -1,2 +1,9 @@
 public class whileloop {
+    public static void main(String[] args){
+        short i = 0;
+        while(i <= 20){
+            System.out.println("Reva University");
+            i++;
+        }
+    }
 }
