8728b29a8b2db754ca6ca2969764fe2796977be0
Shashank B N <shashankbn2023@gmail.com>
2023-09-05 00:02:01 +0530

    added coment

--- src/dowhile.java
+++ src/dowhile.java
@@ -2,7 +2,7 @@ public class dowhile {
     public static void main(String[] args){
         short i = 0;
         do{
-            System.out.println(i);
+            System.out.println(i);//in do while it will perform task later it check the condition
             i++;
         }while(i <= 10);
     }
