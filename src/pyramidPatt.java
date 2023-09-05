d7800af59455def9437b169142cd8925abf2fffd
Shashank B N <shashankbn2023@gmail.com>
2023-09-05 23:01:18 +0530

    Initialised Inverted Pyramid

--- src/pyramidPatt.java
+++ src/pyramidPatt.java
@@ -1,30 +1,30 @@
 import java.util.*;
 //Pyramid Pattern
-public class pyramidPatt {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the value of n :");
-        short n = sc.nextShort();
-        for(short i=1;i<=n;i++){
-            for(short j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-    }
-}
-
-//Inverted pyramid
 //public class pyramidPatt {
 //    public static void main(String[] args){
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter the value of n :");
 //        short n = sc.nextShort();
 //        for(short i=1;i<=n;i++){
-//            for(short j=n;j>=i;j--){
+//            for(short j=1;j<=i;j++){
 //                System.out.print("*");
 //            }
 //            System.out.println();
 //        }
 //    }
 //}
+
+//Inverted pyramid
+public class pyramidPatt {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+        short n = sc.nextShort();
+        for(short i=1;i<=n;i++){
+            for(short j=n;j>=i;j--){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
