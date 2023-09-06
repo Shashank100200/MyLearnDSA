ce07b0089791a9a6d0d7d62ce72fdd0b95b3393f
Shashank B N <shashankbn2023@gmail.com>
2023-09-06 09:24:56 +0530

    Initiated inverted Pyramid num starts ffrom n

--- src/numPattern.java
+++ src/numPattern.java
@@ -14,13 +14,29 @@ import java.util.*;
 //}
 
 //inverted Pyramid
+//public class numPattern {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the value of n :");
+//        short n = sc.nextShort();
+//        In this pyramid number starts from 1-n
+//        for (short i=n; i>=1; i--) {
+//            for (short j=1; j<=i; j++) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
 public class numPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n :");
         short n = sc.nextShort();
-        for (short i=n; i>=1; i--) {
-            for (short j=1; j<=i; j++) {
+        for (short i=1; i<=n; i++) {
+            //it starts number from n-1
+            for (short j=n; j>=i; j--) {
                 System.out.print(j);
             }
             System.out.println();
