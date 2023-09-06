c714b403d280a3465a724b7ceef9209488060f97
Shashank B N <shashankbn2023@gmail.com>
2023-09-06 09:13:36 +0530

    Inverted num Pyramid

--- src/numPattern.java
+++ src/numPattern.java
@@ -1,14 +1,32 @@
 import java.util.*;
+//public class numPattern {
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the value of n :");
+//        short n = sc.nextShort();
+//        for(short i=1;i<=n;i++){
+//            for(short j=1;j<=i;j++){
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+//inverted Pyramid
 public class numPattern {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n :");
         short n = sc.nextShort();
-        for(short i=1;i<=n;i++){
-            for(short j=1;j<=i;j++){
+        for (short i=n; i>=1; i--) {
+            for (short j=1; j<=i; j++) {
                 System.out.print(j);
             }
             System.out.println();
         }
     }
 }
+
+
+
