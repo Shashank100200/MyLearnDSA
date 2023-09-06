af1b521ecaae096abf46a07c647995f4569fcae9
Shashank B N <shashankbn2023@gmail.com>
2023-09-06 08:55:43 +0530

    Initiated number Pyramid

---
+++ src/numPattern.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class numPattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the value of n :");
+        short n = sc.nextShort();
+        for(short i=1;i<=n;i++){
+            for(short j=1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+}
