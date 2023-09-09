3afda6d10e9d6956d2d0aa69760a1a6be73bbd03
Shashank B N <shashankbn2023@gmail.com>
2023-09-10 01:23:07 +0530

    Riverse Number pattern print with right logic

--- src/numPattern.java
+++ src/numPattern.java
@@ -19,7 +19,7 @@ import java.util.*;
 //        Scanner sc = new Scanner(System.in);
 //        System.out.println("Enter the value of n :");
 //        short n = sc.nextShort();
-//        In this pyramid number starts from 1-n
+////        In this pyramid number starts from 1-n
 //        for (short i=n; i>=1; i--) {
 //            for (short j=1; j<=i; j++) {
 //                System.out.print(j);
@@ -29,20 +29,38 @@ import java.util.*;
 //    }
 //}
 
-public class numPattern {
+//public class numPattern {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the value of n :");
+//        short n = sc.nextShort();
+//        for (short i=1; i<=n; i++) {
+//            //it starts number from n-1
+//            for (short j=n; j>=i; j--) {
+//                System.out.print(j);
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+public class numPattern{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the value of n :");
         short n = sc.nextShort();
-        for (short i=1; i<=n; i++) {
-            //it starts number from n-1
-            for (short j=n; j>=i; j--) {
+        for(short i=1;i<=n;i++){
+            //Logic
+            for(short j=1;j<=n-i+1;j++){
                 System.out.print(j);
             }
             System.out.println();
         }
+
     }
 }
+
+
 
 
 
