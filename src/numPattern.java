c44a017d1c4c78e8b8f4e04a3dfba1784ca90dd6
Shashank B N <shashankbn2023@gmail.com>
2023-09-10 02:11:54 +0530

    Addes space for op statement

--- src/numPattern.java
+++ src/numPattern.java
@@ -87,9 +87,9 @@ public class numPattern {
         for(short i=1;i<=n;i++){
             for(short j=1;j<=i;j++){
                 if((i+j)%2 == 0){
-                    System.out.print("1");
+                    System.out.print("1 ");
                 }else{
-                    System.out.print("0");
+                    System.out.print("0 ");
                 }
             }
             System.out.println();
