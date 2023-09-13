90a3c6305e6babf47ad704ff000573122499554c
Shashank B N <shashankbn2023@gmail.com>
2023-09-13 20:14:49 +0530

    Print name using Function

--- src/Functions.java
+++ src/Functions.java
@@ -1,11 +1,27 @@
-public class Functions {
-    public static void funcall(){
-        System.out.println("Shashank"); // function defined
-        return;
+import java.util.*;
+//public class Functions {
+//    public static void funcall(){
+//        System.out.println("Shashank"); // function defined
+//        return;
+//    }
+//    public static void main(String[] args){
+//        funcall();
+//        funcall();
+//        funcall(); //function called
+//    }
+//}
+
+public class Functions{
+    public static void myName(String name){
+        System.out.println("Hello " + name);
+        System.out.println("Bonjore " + name);
     }
     public static void main(String[] args){
-        funcall();
-        funcall();
-        funcall(); //function called
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter your name :");
+        String myNam = sc.nextLine();
+
+        myName(myNam); //Fun call 1
+        myName(myNam +" How are you"); //Fun call 2
     }
 }
