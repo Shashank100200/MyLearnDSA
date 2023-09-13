603dd1c8dddef05bc4c8e7da98fbf044b5971475
Shashank B N <shashankbn2023@gmail.com>
2023-09-13 22:41:10 +0530

    Sum of two numbers using Function

--- src/Functions.java
+++ src/Functions.java
@@ -11,17 +11,37 @@ import java.util.*;
 //    }
 //}
 
-public class Functions{
-    public static void myName(String name){
-        System.out.println("Hello " + name);
-        System.out.println("Bonjore " + name);
+//public class Functions{
+//    public static void myName(String name){
+//        System.out.println("Hello " + name);
+//        System.out.println("Bonjore " + name);
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter your name :");
+//        String myNam = sc.nextLine();
+//
+//        myName(myNam); //Fun call 1
+//        myName(myNam +" How are you"); //Fun call 2
+//    }
+//}
+
+//Sum using Function
+public class Functions {
+    public static int addTwoNum(int a,int b){
+        int sum  = a+b;
+        return sum;
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your name :");
-        String myNam = sc.nextLine();
+        System.out.println("Enter the a value :");
+        short a = sc.nextShort();
+        System.out.println("Enter the b value :");
+        short b = sc.nextShort();
 
-        myName(myNam); //Fun call 1
-        myName(myNam +" How are you"); //Fun call 2
+        int add = addTwoNum(a,b); // Fun Call
+        System.out.println("Sum of two numbers is :"+add);
+
     }
 }
+
