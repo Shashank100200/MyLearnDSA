db043fd8f457d38a6232681649c3f5fc92befa5a
Shashank B N <shashankbn2023@gmail.com>
2023-09-13 20:01:04 +0530

    Initiated fun defination and fun call in program

---
+++ src/Functions.java
@@ -0,0 +1,11 @@
+public class Functions {
+    public static void funcall(){
+        System.out.println("Shashank"); // function defined
+        return;
+    }
+    public static void main(String[] args){
+        funcall();
+        funcall();
+        funcall(); //function called
+    }
+}
