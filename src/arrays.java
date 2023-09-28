76f982d2e417eae3fa44006e70a9af41ad54be09
Shashank B N <shashankbn2023@gmail.com>
2023-09-29 02:02:40 +0530

    initiated linear search program

--- src/arrays.java
+++ src/arrays.java
@@ -15,15 +15,44 @@ import java.util.*;
 //    }
 //}
 
+//user input[size] using array
+//public class arrays{
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the size of array :");
+//        int size = sc.nextInt();
+//        int marks[] = new int[size];
+//        for(int i=0;i<size;i++){
+//            System.out.println(marks[i]);
+//        }
+//    }
+//}
+
 //user input using array
-public class arrays{
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the size of array :");
-        int size = sc.nextInt();
-        int marks[] = new int[size];
-        for(int i=0;i<size;i++){
-            System.out.println(marks[i]);
-        }
-    }
-}
+//public class arrays{
+//    public static void userprint(int[] marks,int size){
+//        System.out.println("Printing the array values :");
+//        for(int i=0;i<size;i++){
+//            System.out.println(marks[i]);
+//        }
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the size of an array :");
+//        int size = sc.nextInt();
+//
+//        //creating array
+//        int marks[] = new int[size];
+//
+//        //getting values
+//        System.out.println("Enter value :");
+//        for(int i=0;i<size;i++){
+//            marks[i] = sc.nextInt();
+//        }
+//        userprint(marks,size );
+//    }
+//}
+
+//linear search
+
+
