f0d2caf5e7774622f49b5b16af5fd3e309f9761b
Shashank B N <shashankbn2023@gmail.com>
2023-09-28 12:30:36 +0530

    Added file named arrays

---
+++ src/arrays.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class arrays {
+    public static void printarr(){
+        int[] marks = new int[3];
+        marks[0] = 35;
+        marks[1] = 38;
+        marks[2] = 36;
+        System.out.println(marks[2]);
+    }
+    public static void main(String[] args){
+        printarr();
+    }
+}
