8ac3443ee217dc470e9728d851d64baffcdd3629
Shashank B N <shashankbn2023@gmail.com>
2023-11-22 01:30:45 +0530

    used String Builder

---
+++ src/practice.java
@@ -0,0 +1,98 @@
+// convert all the upercase to lower and lower to upper
+import java.io.*;
+//import java.util.*;
+//import java.text.*;
+//import java.math.*;
+//import java.util.regex.*;
+//
+//public class practice {
+//        public static void main(String[] args){
+//                Scanner sc = new Scanner(System.in);
+//                System.out.println("Enter the string :");
+//        }
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//public class stringBuilder{
+//        public static String swapCases(String inputString) {
+//            char[] chars = inputString.toCharArray();
+//
+//            for (int i = 0; i < chars.length; i++) {
+//                char currentChar = chars[i];
+//
+//                if (Character.isUpperCase(currentChar)) {
+//                    chars[i] = Character.toLowerCase(currentChar);
+//                } else if (Character.isLowerCase(currentChar)) {
+//                    chars[i] = Character.toUpperCase(currentChar);
+//                }
+//                // For non-alphabetic characters, keep them unchanged
+//            }
+//
+//            return new String(chars);
+//        }
+//
+//        public static void main(String[] args) {
+//            String inputStr = "Hello World";
+//            String result = swapCases(inputStr);
+//            System.out.println(result);
+//        }
+//}
+
+//public class stringBuilder {
+//    public static void main(String[] args){
+//        String inp = "12345";
+//
+//        for(int i=inp.length();i<=0;i--){
+//            System.out.print(i);
+//        }
+//    }
+//}
+
+//        import java.util.Scanner;
+//
+//        import java.io.*;
+//        import java.util.*;
+//        import java.text.*;
+//        import java.math.*;
+//        import java.util.regex.*;
+
+//public class PalindromeCheck {
+//
+//    public static boolean isPalindrome(String s) {
+//        int left = 0;
+//        int right = s.length() - 1;
+//
+//        while (left < right) {
+//            if (s.charAt(left) != s.charAt(right)) {
+//                return false;
+//            }
+//            left++;
+//            right--;
+//        }
+//        return true;
+//    }
+//
+//    public static void main(String[] args) {
+//        String inputStr = "abba";
+//
+//        boolean result = isPalindrome(inputStr);
+//
+//        System.out.println(result);
+//    }
+//}
--- src/stringBuilder.java
+++ src/stringBuilder.java
@@ -1,2 +1,39 @@
+import java.util.*;
 public class stringBuilder {
+    public static void insert(){
+        StringBuilder sb = new StringBuilder("Reva");
+        sb.insert(0,'S');
+        System.out.println(sb);
+    }
+    public static void charAt(){
+        StringBuilder sb = new StringBuilder("Reva");
+        sb.setCharAt(0,'S');
+        System.out.println(sb);
+    }
+    public static void delete(){
+        StringBuilder sb = new StringBuilder("Reva");
+        sb.delete(2,3);
+        System.out.println(sb);
+    }
+    public static void append(){
+        StringBuilder sb = new StringBuilder("Reva");
+        sb.append(" ");
+        sb.append("U");
+        sb.append("niversity");
+        System.out.println(sb);
+        System.out.println(sb.length());
+    }
+    public static void main(String[] args){
+        insert();
+        charAt();
+        delete();
+        append();
+    }
 }
+
+
+
+
+
+
+
