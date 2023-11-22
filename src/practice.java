4b3eb1584d448511b90ba1e1e84943ab3eb4a3fa
Shashank B N <shashankbn2023@gmail.com>
2023-11-23 02:02:33 +0530

    done with Palindrome problem

--- src/practice.java
+++ src/practice.java
@@ -1,49 +1,58 @@
-// convert all the upercase to lower and lower to upper
 import java.io.*;
 import java.util.*;
 import java.text.*;
 import java.math.*;
 import java.util.regex.*;
 
-public class practice {
-        public static void main(String[] args){
-                Scanner sc = new Scanner(System.in);
 
-                //taking input string
-                System.out.println("Enter the input :");
-                String inp = sc.next();
-
-                char[] array = inp.toCharArray();
-
-                for(int i=0;i<array.length;i++){
-                        char curr = array[i];
-                        if(Character.isUpperCase(curr)){
-                                array[i] = Character.toLowerCase(curr);
-                        }else if(Character.isLowerCase(curr)){
-                                array[i] = Character.toUpperCase(curr);
-                        }
-                }
-                System.out.println(new String(array));
-        }
-}
-
-//public class stringBuilder {
-//    public static void main(String[] args){
-//        String inp = "12345";
+// convert all the upercase to lower and lower to upper
+//public class practice {
+//        public static void main(String[] args){
+//                Scanner sc = new Scanner(System.in);
 //
-//        for(int i=inp.length();i<=0;i--){
-//            System.out.print(i);
+//                //taking input string
+//                System.out.println("Enter the input :");
+//                String inp = sc.next();
+//
+//                char[] array = inp.toCharArray();
+//
+//                for(int i=0;i<array.length;i++){
+//                        char curr = array[i];
+//                        if(Character.isUpperCase(curr)){
+//                                array[i] = Character.toLowerCase(curr);
+//                        }else if(Character.isLowerCase(curr)){
+//                                array[i] = Character.toUpperCase(curr);
+//                        }
+//                }
+//                System.out.println(new String(array));
 //        }
-//    }
 //}
 
-//        import java.util.Scanner;
-//
-//        import java.io.*;
-//        import java.util.*;
-//        import java.text.*;
-//        import java.math.*;
-//        import java.util.regex.*;
+public class practice{
+        public static void main(String[] args){
+                Scanner sc = new Scanner(System.in);
+                System.out.println("Enter inp :");
+
+                String inp = sc.next();
+                char[] array = inp.toCharArray();
+
+                Boolean isPalindrome = true;
+
+                for(int i=0;i<=array.length/2;i++){
+                        char curr = array[i];
+                        char curr2 = array[array.length-i-1];
+                                if(curr != curr2 ){
+                                        isPalindrome = false;
+                                        break;
+                                }
+                        }
+                if(isPalindrome){
+                        System.out.println("Palindrome");
+                }else{
+                        System.out.println("Not Palindrome");
+                }
+        }
+}
 
 //public class PalindromeCheck {
 //
