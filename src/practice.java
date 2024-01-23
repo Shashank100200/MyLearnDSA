c4f39b60fe3223f9d6b27fe9b553ba5287ac032d
Shashank B N <shashankbn2023@gmail.com>
2024-01-24 00:13:48 +0530

    changes in string builder

--- src/practice.java
+++ src/practice.java
@@ -397,35 +397,37 @@ import java.math.*;
 //}
 
 //19. Create a program to find the next prime number after a given number.
-public class practice {
-    public static boolean prime(int num){
-        if(num <=1){
-            return false;
-        }
-        for(int i=2;i*i<=num;i++){
-            if(num%i==0){
-                return false;
-            }
-        }
-        return true;
-    }
-    public static int nextPrime(int num){
-        int next = num + 1;
-        while(true){
-            if(prime(next)){
-                return next;
-            }
-            next++;
-        }
-    }
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number :");
-        int num = sc.nextInt();
+//public class practice {
+//    public static boolean prime(int num){
+//        if(num <=1){
+//            return false;
+//        }
+//        for(int i=2;i*i<=num;i++){
+//            if(num%i==0){
+//                return false;
+//            }
+//        }
+//        return true;
+//    }
+//    public static int nextPrime(int num){
+//        int next = num + 1;
+//        while(true){
+//            if(prime(next)){
+//                return next;
+//            }
+//            next++;
+//        }
+//    }
+//    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Enter the number :");
+//        int num = sc.nextInt();
+//
+//        System.out.println(nextPrime(num));
+//    }
+//}
 
-        System.out.println(nextPrime(num));
-    }
-}
+
 
 
 
