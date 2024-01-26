d4c67f1de2ce945f63bd8174d47a2b1f76fc10f4
Shashank B N <shashankbn2023@gmail.com>
2024-01-26 12:44:09 +0530

    program to reverse an array

--- src/PracticeInArrays.java
+++ src/PracticeInArrays.java
@@ -1,13 +1,26 @@
 import java.util.*;
 //1. Write an algorithm to find the sum of all elements in an array.
+//public class PracticeInArrays {
+//    public static void main(String[] args){
+//        int[] array={1,3,5,7,9};
+//
+//        int sum=0;
+//        for(int i=0;i<array.length;i++){
+//            sum = sum + array[i];
+//        }
+//        System.out.println(sum);
+//    }
+//}
+
+
+//2. Implement a function to reverse the elements of an array.
 public class PracticeInArrays {
     public static void main(String[] args){
-        int[] array={1,3,5,7,9};
+        int[] array={1,2,3,4,5};
 
-        int sum=0;
-        for(int i=0;i<array.length;i++){
-            sum = sum + array[i];
+        for(int i= array.length-1;i>=0;i--){
+            System.out.print(array[i]+" ");
         }
-        System.out.println(sum);
     }
 }
+
