1db18eb6a1b4bfa38ef29526371d06bb6f5f111e
Shashank B N <shashankbn2023@gmail.com>
2024-01-27 00:31:55 +0530

    implemented bitwise set opwrator

--- src/BitManupulation.java
+++ src/BitManupulation.java
@@ -1,14 +1,27 @@
 //Get bit operator
+//public class BitManupulation {
+//    public static void main(String[] args){
+//        int num = 6;
+//        int pos = 1 << 2;
+//
+//        if((pos & num)==0){
+//            System.out.println("Bit was zero");
+//        }else{
+//            System.out.println("Bit was one");
+//        }
+//    }
+//}
+
+//Set bit operator
 public class BitManupulation {
     public static void main(String[] args){
-        int num = 6;
-        int pos = 1 << 2;
+        int num = 7;
+        int pos = 3;
+        int bitmask = 1<<pos;
 
-        if((pos & num)==0){
-            System.out.println("Bit was zero");
-        }else{
-            System.out.println("Bit was one");
+        int newNum = num | bitmask;
+        System.out.println(newNum);
         }
-    }
 }
+
 
