1ee420150495c4a3a99c811efcb3bff4742d116d
Shashank B N <shashankbn2023@gmail.com>
2024-01-27 00:24:49 +0530

    implemented bitwise get operator

--- src/BitManupulation.java
+++ src/BitManupulation.java
@@ -1,2 +1,14 @@
+//Get bit operator
 public class BitManupulation {
+    public static void main(String[] args){
+        int num = 6;
+        int pos = 1 << 2;
+
+        if((pos & num)==0){
+            System.out.println("Bit was zero");
+        }else{
+            System.out.println("Bit was one");
+        }
+    }
 }
+
