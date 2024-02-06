63886b9a76548667f9a7b96833b8f400688c752f
Shashank B N <shashankbn2023@gmail.com>
2024-02-06 23:12:59 +0530

    Implemented program to print numbers 1-5

--- src/recusrsion.java
+++ src/recusrsion.java
@@ -1,12 +1,25 @@
+//public class recusrsion {
+//    public static void printNum(int n){
+//        if(n == 0){
+//            return;
+//        }
+//        System.out.println(n);
+//        printNum(n-1);
+//    }
+//    public static void main(String[] args){
+//        printNum(5);
+//    }
+//}
+
 public class recusrsion {
     public static void printNum(int n){
-        if(n == 0){
+        if(n > 5){
             return;
         }
         System.out.println(n);
-        printNum(n-1);
+        printNum(n+1);
     }
     public static void main(String[] args){
-        printNum(5);
+        printNum(1);
     }
 }
