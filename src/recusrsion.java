c7de63cd65b87a46ba0155b285b219cf65fc347b
Shashank B N <shashankbn2023@gmail.com>
2024-02-24 00:18:35 +0530

    Implemented program to print power on n using recursion

--- src/recusrsion.java
+++ src/recusrsion.java
@@ -124,27 +124,65 @@
 //    }
 //}
 
-//Fibonacchi of a nth number
+//2. Implement a recursive function to find the nth Fibonacci number.
+//public class recusrsion {
+//    static int fib(int n){
+//        if(n == 1){
+//            return 1;
+//        }
+//        if(n == 2){
+//            return 1;
+//        }
+//        int fibans1 = fib(n-1);
+//        int fibans2 = fib(n-2);
+//        return fibans1 + fibans2;
+//    }
+//
+//    public static void main(String[] args) {
+//        int ans = fib(5);
+//        System.out.println(ans);
+//    }
+//}
+
+//6. Implement a recursive function to calculate the power of a number.
+//public class recusrsion {
+//    static int power(int x, int n){
+//        if( x == 0){
+//            return 0;
+//        }
+//        if(n == 0){
+//            return 1;
+//        }
+//        int ans1 = power(x,n-1);
+//        return x * ans1;
+//    }
+//
+//    public static void main(String[] args) {
+//        int ans = power(2,3);
+//        System.out.println(ans);
+//    }
+//}
+
 public class recusrsion {
-    static int fib(int n){
-        if(n == 1){
+    static int power(int x,int n){
+        if(x == 0){
+            return 0;
+        }
+        if(n == 0){
             return 1;
         }
-        if(n == 2){
-            return 1;
+        int temp = power(x,n/2);
+        if(n%2 == 1){
+            return temp*temp*x;
         }
-        int fibans1 = fib(n-1);
-        int fibans2 = fib(n-2);
-        return fibans1 + fibans2;
+        return temp*temp;
     }
 
     public static void main(String[] args) {
-        int ans = fib(5);
+        int ans = power(2,5);
         System.out.println(ans);
     }
 }
-
-
 
 
 
