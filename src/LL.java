4f0a7392f51da84486ef3dafcfe0ebe0bb8f4f51
Shashank B N <shashankbn2023@gmail.com>
2024-04-14 08:45:36 +0530

    Implemented a program to delete element of linked list from front and last

--- src/LL.java
+++ src/LL.java
@@ -1,17 +1,18 @@
 import java.util.*;
 public class LL {
     Node head;  // Declairation of head
+    private int size;
     class Node{  // Creating class
         String data;  //variables of the class
         Node next;
 
-        Node(String data){   //Constructor
+        Node(String data){            //Constructor
             this.data = data;
             this.next = null;
         }
     }
 
-    //adding the element of LL at first
+    //adding the element to LL at first
     public void addFirst(String data){
         Node newNode= new Node(data);  //Creating new node
         if(head == null){              //Checking weather LL is empty
@@ -21,6 +22,8 @@ public class LL {
         newNode.next = head;           //linking next of new node to current node(1st node) where head is pointing
         head = newNode;                //Making new node as a head
     }
+
+    //adding the ele to LL at last
     public void addLast(String data){
         Node newNode = new Node(data); //Creating new node
         if(head == null){              //Checking weather LL is empty
@@ -33,19 +36,50 @@ public class LL {
         }
         currNode.next = newNode;       //after it reaches the last node assigning its next to new node
     }
+
+    //to print the ele of the list
     public void printList(){
-        if(head == null){              //Creating new node
+        if(head == null){              //Checking weather the LL is empty
             System.out.println("Empty List"); //printing empty if list is empty
             return;
         }                               //else
         Node lastNode = head;           //creating currNode named variable which acts like pointer to traverse assining as head to it
-        while(lastNode.next != null){
+        while(lastNode != null){
             System.out.print(lastNode.data+"->"); // printing each element while traverse
             lastNode = lastNode.next;   //making it traverse where we use lastNode.next
         }
         System.out.println("Null");     //printing null node (last node)
     }
 
+//    deleting the ele at first
+    public void deleteFirst(){
+        if(head == null){               //Checking weather the LL is empty
+            System.out.println("List is empty");
+            return;
+        }
+        head = head.next;               //meaking 2nd node as head where 1st node is lost now there fore the node is deleted
+    }
+
+//    deleting the ele at the last
+    public void deleteLast(){
+        if(head == null){               //Checking weather the LL is empty
+            System.out.println("List is empty");
+            return;
+        }
+        if(head.next == null){          //head.next == null -> lastnode = null ,then null.next means lastnode.next will get error
+            head = null;                //to avoid this we will satisfy this corner case
+            return;
+        }
+
+        Node lastnode = head.next;      //creating variable lastnode as head.next
+        Node seclasnode = head;         //and seclast as head
+
+        while(lastnode.next != null){   //till lastnode != null
+            lastnode = lastnode.next;   //it will traverse
+            seclasnode = seclasnode.next; //and the seclast will also traverse till lastnode will reach last node and seclast will be at last but one node
+        }
+        seclasnode.next = null;         //linking seclasnode next to null
+    }
     public static void main(String[] args) {
         LL list = new LL();
         list.addFirst("a");
@@ -54,6 +88,10 @@ public class LL {
         list.printList();
         list.addLast("Reva");
         list.addLast("University");
+        list.printList();
+        list.deleteFirst();
+        list.printList();
+        list.deleteLast();
         list.printList();
     }
 }
