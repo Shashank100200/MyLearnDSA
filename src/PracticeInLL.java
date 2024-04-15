3e274029d7f450d7684f86212f13587faf224898
Shashank B N <shashankbn2023@gmail.com>
2024-04-15 21:56:31 +0530

    Implemented program to find a delete an ele in a LL above 25

--- src/PracticeInLL.java
+++ src/PracticeInLL.java
@@ -2,31 +2,56 @@ import java.util.*;
 
 //Make a Linked List & add the following elements to it : (1, 5, 7, 3 , 8, 2, 3). Search for the number 7 & display its index.
 
+//public class PracticeInLL {
+//    public static void main(String[] args) {
+//        LinkedList<Integer> list = new LinkedList<Integer>();
+//
+//        list.add(1);
+//        list.add(5);
+//        list.add(7);
+//        list.add(3);
+//        list.add(8);
+//        list.add(2);
+//        list.add(3);
+//
+//        int ans = -1;
+//
+//        for(int i=0;i<list.size();i++){
+//            int curr = list.get(i);
+//            if(curr == 7){
+//                ans = i;
+//                break;
+//            }
+//        }
+//        if(ans == -1){
+//            System.out.println("Ele not found");
+//        }else{
+//            System.out.println("ele found at index :"+ans);
+//        }
+//    }
+//}
+
+//Take elements(numbers in the range of 1-50) of a Linked List as input from the user. Delete all nodes which have values greater than 25.
 public class PracticeInLL {
     public static void main(String[] args) {
         LinkedList<Integer> list = new LinkedList<Integer>();
 
-        list.add(1);
-        list.add(5);
-        list.add(7);
-        list.add(3);
-        list.add(8);
-        list.add(2);
-        list.add(3);
-
-        int ans = -1;
+        list.add(22);
+        list.add(25);
+        list.add(13);
+        list.add(27);
+        list.add(21);
+        list.add(100);
+        list.add(14);
 
         for(int i=0;i<list.size();i++){
             int curr = list.get(i);
-            if(curr == 7){
-                ans = i;
-                break;
+
+            if(curr > 25){
+                list.remove(i);
             }
         }
-        if(ans == -1){
-            System.out.println("Ele not found");
-        }else{
-            System.out.println("ele found at index :"+ans);
-        }
+        System.out.println(list);
+        System.out.println(list.size());
     }
 }
