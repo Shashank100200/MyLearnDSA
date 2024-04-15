6a334f73d71389f0dbf85dddeac1a380142d49bd
Shashank B N <shashankbn2023@gmail.com>
2024-04-15 19:22:46 +0530

    Implemented program to find a ele in a LL

--- src/PracticeInLL.java
+++ src/PracticeInLL.java
@@ -1,2 +1,32 @@
+import java.util.*;
+
+//Make a Linked List & add the following elements to it : (1, 5, 7, 3 , 8, 2, 3). Search for the number 7 & display its index.
+
 public class PracticeInLL {
+    public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<Integer>();
+
+        list.add(1);
+        list.add(5);
+        list.add(7);
+        list.add(3);
+        list.add(8);
+        list.add(2);
+        list.add(3);
+
+        int ans = -1;
+
+        for(int i=0;i<list.size();i++){
+            int curr = list.get(i);
+            if(curr == 7){
+                ans = i;
+                break;
+            }
+        }
+        if(ans == -1){
+            System.out.println("Ele not found");
+        }else{
+            System.out.println("ele found at index :"+ans);
+        }
+    }
 }
