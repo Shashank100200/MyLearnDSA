2e75fb87ddef40533e7e5d75ca5787948fdb1c3b
Shashank B N <shashankbn2023@gmail.com>
2024-04-16 22:07:56 +0530

    Incerted new file named reverseOfLL

--- src/PracticeInLL.java
+++ src/PracticeInLL.java
@@ -32,26 +32,46 @@ import java.util.*;
 //}
 
 //Take elements(numbers in the range of 1-50) of a Linked List as input from the user. Delete all nodes which have values greater than 25.
-public class PracticeInLL {
-    public static void main(String[] args) {
-        LinkedList<Integer> list = new LinkedList<Integer>();
+//public class PracticeInLL {
+//    public static void main(String[] args) {
+//        LinkedList<Integer> list = new LinkedList<Integer>();
+//
+//        list.add(22);
+//        list.add(25);
+//        list.add(13);
+//        list.add(27);
+//        list.add(21);
+//        list.add(100);
+//        list.add(14);
+//
+//        for(int i=0;i<list.size();i++){
+//            int curr = list.get(i);
+//
+//            if(curr > 25){
+//                list.remove(i);
+//            }
+//        }
+//        System.out.println(list);
+//        System.out.println(list.size());
+//    }
+//}
 
-        list.add(22);
-        list.add(25);
-        list.add(13);
-        list.add(27);
-        list.add(21);
-        list.add(100);
-        list.add(14);
+//5.Write a Java program to find the middle element of a linked list.
 
-        for(int i=0;i<list.size();i++){
-            int curr = list.get(i);
 
-            if(curr > 25){
-                list.remove(i);
-            }
-        }
-        System.out.println(list);
-        System.out.println(list.size());
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
