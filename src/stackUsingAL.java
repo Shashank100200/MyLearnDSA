609ba190792c81022972ab00e0e855082250d879
Shashank B N <shashankbn2023@gmail.com>
2024-06-18 13:31:00 +0530

    Implementation of stack using Array List using collection frameworks

--- src/stackUsingAL.java
+++ src/stackUsingAL.java
@@ -1,41 +1,58 @@
 import java.util.*;
+//public class stackUsingAL {
+//    static class Stack{
+//        public static ArrayList<Integer> list = new ArrayList<>();
+//        public static boolean isEmpty(){
+//            return list.size() == 0;
+//        }
+//        public static void push(int data){
+//            list.add(data);
+//        }
+//
+//        public static int pop(){
+//            if(isEmpty()){
+//                return -1;
+//            }
+//            int top = list.get(list.size() - 1);
+//            list.remove(list.size() - 1);
+//            return top;
+//        }
+//
+//        public static int peek(){
+//            if(isEmpty()){
+//                return -1;
+//            }
+//            return list.get(list.size() - 1);
+//        }
+//    }
+//
+//    public static void main(String[] args) {
+//        Stack s = new Stack();
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//        s.push(4);
+//
+//        while(!s.isEmpty()){
+//            System.out.println(s.peek());
+//            s.pop();
+//        }
+//    }
+//}
+
 public class stackUsingAL {
-    static class Stack{
-        public static ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty(){
-            return list.size() == 0;
-        }
-        public static void push(int data){
-            list.add(data);
-        }
+        public static void main(String[] args) {
+            Stack<Integer> s = new Stack<>();
+            s.push(1);
+            s.push(2);
+            s.push(3);
+            s.push(4);
 
-        public static int pop(){
-            if(isEmpty()){
-                return -1;
+            while(!s.isEmpty()){
+                System.out.println(s.peek());
+                s.pop();
             }
-            int top = list.get(list.size() - 1);
-            list.remove(list.size() - 1);
-            return top;
         }
-
-        public static int peek(){
-            if(isEmpty()){
-                return -1;
-            }
-            return list.get(list.size() - 1);
-        }
-    }
-
-    public static void main(String[] args) {
-        Stack s = new Stack();
-        s.push(1);
-        s.push(2);
-        s.push(3);
-        s.push(4);
-
-        while(!s.isEmpty()){
-            System.out.println(s.peek());
-            s.pop();
-        }
-    }
 }
+
+
