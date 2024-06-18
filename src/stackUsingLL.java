4d8839e9900e6139b1fb992e058592b2fc61c822
Shashank B N <shashankbn2023@gmail.com>
2024-06-18 12:55:48 +0530

    Implementation of stack using Linked List

--- src/stackUsingLL.java
+++ src/stackUsingLL.java
@@ -1,4 +1,64 @@
 import java.util.*;
 public interface stackUsingLL {
+    static class Node{
+        int data;
+        Node next;
+        public Node(int data){
+            this.data = data;
+            next = null;
+        }
+    }
 
+    //Stack Class
+    static class stack{
+        public static Node head;
+
+        public static boolean isEmpty(){
+            return head == null;
+        }
+
+        //Push Operation
+        public static void push(int data){
+            Node newNode = new Node(data);
+
+            if(isEmpty()){
+                head = newNode;
+                return;
+            }
+            newNode.next = head;
+            head = newNode;
+        }
+
+        //Pop Operation
+        public static int pop(){
+            if(isEmpty()){
+                return -1;
+            }
+            int top = head.data;
+            head = head.next;
+            return top;
+        }
+
+        //Peek Operation
+        public static int peek(){
+            if(isEmpty()){
+                return -1;
+            }
+            return head.data;
+        }
+    }
+
+    public static void main(String[] args) {
+       Stack s = new Stack();
+
+       s.push(1);
+       s.push(2);
+       s.push(3);
+        s.push(4);
+
+        while(!s.isEmpty()){
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
 }
