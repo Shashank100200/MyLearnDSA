806b85c1773304a0438a81cfd9e53bfde85bb143
Shashank B N <shashankbn2023@gmail.com>
2024-06-23 12:11:26 +0530

    Reversing element in stack

--- src/stackUsingAL.java
+++ src/stackUsingAL.java
@@ -57,6 +57,32 @@ import java.util.*;
 
 
 //push the ele at the bottom of the stack
+//public class stackUsingAL {
+//    public static void pushAtBottom(int data,Stack<Integer> s){
+//        if(s.isEmpty()){
+//            s.push(data);
+//            return;
+//        }
+//        int top = s.pop();
+//        pushAtBottom(data,s);
+//        s.push(top);
+//    }
+//
+//    public static void main(String[] args) {
+//        Stack<Integer> s = new Stack<>();
+//        s.push(1);
+//        s.push(2);
+//        s.push(3);
+//
+//        pushAtBottom(4,s);
+//
+//        while(!s.isEmpty()){
+//            System.out.println(s.peek());
+//            s.pop();
+//        }
+//    }
+//}
+
 public class stackUsingAL {
     public static void pushAtBottom(int data,Stack<Integer> s){
         if(s.isEmpty()){
@@ -68,13 +94,22 @@ public class stackUsingAL {
         s.push(top);
     }
 
+    public static void reverse(Stack<Integer> s){
+        if(s.isEmpty()){
+            return;
+        }
+        int top = s.pop();
+        reverse(s);
+        pushAtBottom(top,s);
+    }
+
     public static void main(String[] args) {
         Stack<Integer> s = new Stack<>();
         s.push(1);
         s.push(2);
         s.push(3);
 
-        pushAtBottom(4,s);
+        reverse(s);
 
         while(!s.isEmpty()){
             System.out.println(s.peek());
