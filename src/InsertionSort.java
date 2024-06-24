14b1b98a4c2e1eb12f33fc6f14246a32922e0df2
Shashank B N <shashankbn2023@gmail.com>
2024-06-24 16:51:02 +0530

    Implemented code for Insertion sort

--- src/InsertionSort.java
+++ src/InsertionSort.java
@@ -1,4 +1,19 @@
 import java.util.*;
 public class InsertionSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
 
+        for(int i=1;i< arr.length;i++){
+            int curr = arr[i];
+            int j = i-1;
+            while(j >= 0 && curr < arr[j]){
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j+1] = curr;
+        }
+        for(int i=0;i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
