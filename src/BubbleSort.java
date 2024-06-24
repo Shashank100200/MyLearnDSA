bc26a8fa6c1c784450da29c88b08f6ef6cb3a266
Shashank B N <shashankbn2023@gmail.com>
2024-06-24 16:15:52 +0530

    Implemented code for Bubble sort

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,3 +1,19 @@
 import java.util.*;
 public class BubbleSort {
+    public static void main(String[] args) {
+        int arr[] = {5,4,3,2,1};
+
+        for(int i=0;i< arr.length;i++){
+            for(int j=0;j< arr.length-i-1;j++){
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        for(int i=0;i< arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
 }
