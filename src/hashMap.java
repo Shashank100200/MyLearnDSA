0e5604100f9045b96f1c51b10d9407d357efbacf
Shashank B N <shashankbn2023@gmail.com>
2024-07-06 15:58:14 +0530

    Inserted class named ForEachLoop

--- src/hashMap.java
+++ src/hashMap.java
@@ -1,2 +1,27 @@
+import java.util.*;
+
+//      Same as HashSet it not contains duplicate values
 public class hashMap {
+    public static void main(String[] args) {
+        HashMap<String,Integer> map = new HashMap<>();
+
+        map.put("India",122);
+        map.put("Chaina",130);
+        map.put("USA",32);
+        map.put("UK",45);
+        map.put("India",122);
+
+        System.out.println(map);
+        System.out.println(map.size());
+        System.out.println(map.get("Chaina"));
+
+        //Searching in Map
+
+        if(map.containsKey("India")){
+            System.out.println("Contains");
+        }else{
+            System.out.println("Not contains");
+        }
+
+    }
 }
