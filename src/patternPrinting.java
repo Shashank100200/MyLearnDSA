8ef031500f44d8f9d19d6c93132b15ac9b818271
Shashank BN <shashankbn2023@gmail.com>
2024-09-18 10:57:41 +0530

    inserted program to find decreasing pyramid pattern

--- src/patternPrinting.java
+++ src/patternPrinting.java
@@ -53,22 +53,40 @@ import java.util.*;
 //                                                          ****
 //                                                         *****
 
+//public class patternPrinting {
+//    public static void main(String[] args) {
+//        for(int i=1;i<=5;i++){
+//            for(int j=i;j<=5;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
+
+//                                                               *****
+//                                                                ****
+//                                                                 ***
+//                                                                  **
+//                                                                   *
+
+
 public class patternPrinting {
     public static void main(String[] args) {
         for(int i=1;i<=5;i++){
-            for(int j=i;j<=5;j++){
+            for(int j=1;j<=i;j++){
                 System.out.print("  ");
             }
-            for(int j=1;j<=i;j++){
+            for(int j=i;j<=5;j++){
                 System.out.print("* ");
             }
             System.out.println();
         }
     }
 }
-
-
-
 
 
 
