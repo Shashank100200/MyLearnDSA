0890c8ddb428269ec33cad2f7b836d2a55464fe4
Shashank BN <shashankbn2023@gmail.com>
2024-09-18 11:59:57 +0530

    inserted program to print Floyds triangle

--- src/patternPrinting.java
+++ src/patternPrinting.java
@@ -117,17 +117,72 @@ import java.util.*;
 //                                                               * * * * *
 //                                                                 * * *
 //                                                                   *
+//public class patternPrinting {
+//  public static void main(String[] args) {
+//    for(int i=1;i<=5;i++){
+//      for(int j=1;j<=i;j++){
+//        System.out.print("  ");
+//      }
+//      for(int j=i;j<=4;j++){
+//        System.out.print("* ");
+//      }
+//      for(int j=i;j<=5;j++){
+//        System.out.print("* ");
+//      }
+//      System.out.println();
+//    }
+//  }
+//}
+
+
+//                                                                  *
+//                                                                * * *
+//                                                              * * * * *
+//                                                            * * * * * * *
+//                                                          * * * * * * * * *
+//                                                            * * * * * * *
+//                                                              * * * * *
+//                                                                * * *
+//                                                                  *
+
+//public class patternPrinting {
+//  public static void main(String[] args) {
+//    for(int i=1;i<5;i++){
+//      for(int j=i;j<=5;j++){
+//        System.out.print("  ");
+//      }
+//      for(int j=1;j<i;j++){
+//        System.out.print("* ");
+//      }
+//      for(int j=1;j<=i;j++){
+//        System.out.print("* ");
+//      }
+//      System.out.println();
+//    }
+//    for(int i=1;i<=5;i++){
+//      for(int j=1;j<=i;j++){
+//        System.out.print("  ");
+//      }
+//      for(int j=i;j<5;j++){
+//        System.out.print("* ");
+//      }
+//      for(int j=i;j<=5;j++){
+//        System.out.print("* ");
+//      }
+//      System.out.println();
+//    }
+//  }
+//}
+
+
+//                              Floyed's Triangle
 public class patternPrinting {
   public static void main(String[] args) {
+    int num = 1;
     for(int i=1;i<=5;i++){
       for(int j=1;j<=i;j++){
-        System.out.print("  ");
-      }
-      for(int j=i;j<=4;j++){
-        System.out.print("* ");
-      }
-      for(int j=i;j<=5;j++){
-        System.out.print("* ");
+        System.out.print(num+" ");
+        num++;
       }
       System.out.println();
     }
