3255c0e5d605df12bda20702418dfc20290b9ed9
Shashank BN <shashankbn2023@gmail.com>
2024-09-18 11:15:38 +0530

    inserted program to print actual pyramid pattern

--- src/patternPrinting.java
+++ src/patternPrinting.java
@@ -74,20 +74,43 @@ import java.util.*;
 //                                                                   *
 
 
-public class patternPrinting {
-    public static void main(String[] args) {
-        for(int i=1;i<=5;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("  ");
-            }
-            for(int j=i;j<=5;j++){
-                System.out.print("* ");
-            }
-            System.out.println();
-        }
-    }
-}
+//public class patternPrinting {
+//    public static void main(String[] args) {
+//        for(int i=1;i<=5;i++){
+//            for(int j=1;j<=i;j++){
+//                System.out.print("  ");
+//            }
+//            for(int j=i;j<=5;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//    }
+//}
 
+//                                                                    *
+//                                                                   ***
+//                                                                  *****
+//                                                                 *******
+//                                                                *********
+
+
+public class patternPrinting {
+  public static void main(String[] args) {
+    for(int i=1;i<=5;i++){
+      for(int j=i;j<=5;j++){
+        System.out.print("  ");
+      }
+      for(int j=1;j<i;j++){
+        System.out.print("* ");
+      }
+      for(int j=1;j<=i;j++){
+        System.out.print("* ");
+      }
+      System.out.println();
+    }
+  }
+}
 
 
 
