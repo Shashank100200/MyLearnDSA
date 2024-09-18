1a6e391737640fb75f6592ae477b33f53fd30b7f
Shashank BN <shashankbn2023@gmail.com>
2024-09-18 11:34:36 +0530

    inserted program to print actual inversse pyramid pattern

--- src/patternPrinting.java
+++ src/patternPrinting.java
@@ -95,16 +95,38 @@ import java.util.*;
 //                                                                *********
 
 
+//public class patternPrinting {
+//  public static void main(String[] args) {
+//    for(int i=1;i<=5;i++){
+//      for(int j=i;j<=5;j++){
+//        System.out.print("  ");
+//      }
+//      for(int j=1;j<i;j++){
+//        System.out.print("* ");
+//      }
+//      for(int j=1;j<=i;j++){
+//        System.out.print("* ");
+//      }
+//      System.out.println();
+//    }
+//  }
+//}
+
+//                                                           * * * * * * * * *
+//                                                             * * * * * * *
+//                                                               * * * * *
+//                                                                 * * *
+//                                                                   *
 public class patternPrinting {
   public static void main(String[] args) {
     for(int i=1;i<=5;i++){
-      for(int j=i;j<=5;j++){
+      for(int j=1;j<=i;j++){
         System.out.print("  ");
       }
-      for(int j=1;j<i;j++){
+      for(int j=i;j<=4;j++){
         System.out.print("* ");
       }
-      for(int j=1;j<=i;j++){
+      for(int j=i;j<=5;j++){
         System.out.print("* ");
       }
       System.out.println();
